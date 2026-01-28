f96aa575605835fae1690028a3f7dff3b08085c1
romel <romelmahmud.dev@gmail.com>
2026-01-28 20:10:27 +0600

    sign in and sign up using email and password

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
+import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
+app.use(
+  cors({
+    origin: process.env.APP_URL || "http://localhost:4000",
+    credentials: true,
+  }),
+);
+app.use(express.urlencoded());
 
 app.use(express.json());
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use("/", (req: Request, res: Response) => {
   res.json({ message: "Welcome MediStore Home" });
