b152fe03448ea009f4bbf47e9457930e67f5a662
romel <romelmahmud.dev@gmail.com>
2026-02-04 23:23:32 +0600

    disable email verification

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import { auth } from "./lib/auth";
 import { errorHandler } from "./middleware/errorHandler";
 import { categoryRouter } from "./modules/category/category.route";
@@ -18,13 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
-// app.use("/", (req: Request, res: Response) => {
-//   res.json({ message: "Welcome MediStore Home" });
-// });
-
 app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/medicines", medicineRouter);
 app.use("/api/v1/users", userRouter);
+
+app.use("/", (req: Request, res: Response) => {
+  res.json({ message: "Welcome MediStore Home" });
+});
 
 app.use(errorHandler);
 
